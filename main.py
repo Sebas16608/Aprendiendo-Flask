be7fa72df8413c0323cee1e8c6141887a32f84e6
Sebas16608 <asebasrr444@gmail.com>
2025-06-15 19:57:49 -0600

    Aprendi bootstrap y WTF

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+
+app.config["SECRET_KEY"] = "S3b@sr1234"
+
 items = ["Arroz", "Huevos", "Café", "Leche"]
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Nombre del Usuario")
+    password = PasswordField("Contraseña")
+    submit = SubmitField("Enviar datos")
+
+
 
 @app.errorhandler(404)
 def not_found_endpoint(error):
@@ -13,15 +28,17 @@ def not_found_endpoint(error):
 def index():
     user_ip_information = request.remote_addr
     response = make_response(  redirect("/show-information-address"))
-    response.set_cookie("user_ip_information", user_ip_information)
+    session["user_ip_information"] = user_ip_information
     return response
 
-@app.route("/show-information-address")
+@app.route("/show-information-address", methods = ["GET", "POST"])
 def show_information():
-    user_ip = request.cookies.get("user_ip_information")
+    user_ip = session.get("user_ip_information")
+    login_form = LoginForm()
     context = {
         "user_ip": user_ip,
-        "items": items
+        "items": items,
+        "login_form": login_form
     }
     return render_template("ip_information.html", **context)
 
