bd4c11b027de7015553fbc5f31fabefb4918c232
Sebas16608 <asebasrr444@gmail.com>
2025-06-15 21:35:51 -0600

    Implementando mensajes flash con flask

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
@@ -47,7 +47,8 @@ def show_information():
     if login_form.validate_on_submit():
         username = login_form.username.data
         session["username"] = username
-        return make_response(redirect("/index"))
+        flash("Nombre de usuario registrado correctamente")
+        return redirect(url_for("index"))
     return render_template("ip_information.html", **context)
 
 
